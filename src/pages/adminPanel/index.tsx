871362f56ed938f2d12f2ec8e5c93fcf81a7314b
Misha Kompaniec <mikokompaniec@gmail.com>
2025-07-08 17:39:44 +0200

    add validation

--- src/pages/adminPanel/index.tsx
+++ src/pages/adminPanel/index.tsx
@@ -1,17 +1,34 @@
-import { Table, Button, Popconfirm } from 'antd';
+import {
+  Table,
+  Button,
+  Popconfirm,
+  Modal,
+  Form,
+  Input,
+  InputNumber,
+  Select,
+} from 'antd';
 import { Btn, Wrapper } from './style';
 import type { BasketItem } from '../../types';
 import {
+  useCreateProductMutation,
   useDeleteProductMutation,
   useGetProductsQuery,
 } from '../../services/productsApi';
 import { DeleteOutlined } from '@ant-design/icons';
 import { useTranslation } from 'react-i18next';
+import { useState } from 'react';
+
+const { Option } = Select;
 
 const AdminPanel = () => {
   const { t } = useTranslation();
   const { data: products } = useGetProductsQuery();
   const [deleteProduct] = useDeleteProductMutation();
+  const [addProduct] = useCreateProductMutation();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [form] = Form.useForm();
 
   const handleDelete = async (id: string) => {
     try {
@@ -21,7 +38,24 @@ const AdminPanel = () => {
     }
   };
 
-  const handleAdd = () => {};
+  const handleAdd = () => {
+    form.resetFields();
+    setIsModalOpen(true);
+  };
+
+  const handleModalOk = async () => {
+    try {
+      const newProduct = await form.validateFields();
+      addProduct(newProduct);
+      setIsModalOpen(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleModalCancel = () => {
+    setIsModalOpen(false);
+  };
 
   const columns = [
     {
@@ -67,12 +101,75 @@ const AdminPanel = () => {
       <Btn type='primary' size='large' onClick={handleAdd}>
         {t('adminPanel.addProduct')}
       </Btn>
+
       <Table
         dataSource={products}
         columns={columns}
         rowKey='id'
         pagination={false}
       />
+
+      <Modal
+        title={t('adminPanel.modalTitle')}
+        open={isModalOpen}
+        onOk={handleModalOk}
+        onCancel={handleModalCancel}
+        okText={t('adminPanel.save')}
+        cancelText={t('adminPanel.cancel')}
+      >
+        <Form form={form} layout='vertical'>
+          <Form.Item
+            label={t('adminPanel.title')}
+            name='title'
+            rules={[
+              { required: true, message: t('adminPanel.required') },
+              { max: 30, message: t('adminPanel.titleTooLong') }, // добавить перевод
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label={t('adminPanel.price')}
+            name='price'
+            rules={[
+              { required: true, message: t('adminPanel.required') },
+              {
+                type: 'number',
+                max: 100000,
+                message: t('adminPanel.priceTooHigh'), // добавить перевод
+              },
+            ]}
+          >
+            <InputNumber style={{ width: '100%' }} min={0} />
+          </Form.Item>
+
+          <Form.Item
+            label={t('adminPanel.description')}
+            name='description'
+            rules={[
+              { required: true, message: t('adminPanel.required') },
+              { max: 50, message: t('adminPanel.descriptionTooLong') }, // добавить перевод
+            ]}
+          >
+            <Input.TextArea rows={3} />
+          </Form.Item>
+
+          <Form.Item
+            label={t('adminPanel.category')}
+            name='category'
+            rules={[{ required: true, message: t('adminPanel.required') }]}
+          >
+            <Select>
+              <Option value='bouquets'>{t('adminPanel.bouquets')}</Option>
+              <Option value='plants'>{t('adminPanel.plants')}</Option>
+              <Option value='fruitBouquets'>
+                {t('adminPanel.fruitBouquets')}
+              </Option>
+            </Select>
+          </Form.Item>
+        </Form>
+      </Modal>
     </Wrapper>
   );
 };
