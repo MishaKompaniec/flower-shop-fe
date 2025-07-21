bf6803cf9d8f6c9fb5c44c38de0882839c456605
Misha Kompaniec <mikokompaniec@gmail.com>
2025-07-21 16:24:15 +0200

    change Instagram block

--- src/pages/home/instagram/index.tsx
+++ src/pages/home/instagram/index.tsx
@@ -1,45 +1,29 @@
 import { useTranslation } from 'react-i18next';
-
-import {
-  FlexWrapper,
-  ImgWrapper,
-  ImageItem,
-  ImgBlock,
-  Image1,
-  Title,
-  Inst,
-} from './style';
+import { Container, Title, Grid, LargeImage, SmallImage, Inst } from './style';
 
 const Instagram = () => {
   const { t } = useTranslation();
 
   return (
-    <>
+    <Container>
       <Title>
-        {t('instagram.title1')}
+        {t('instagram.title1')}{' '}
         <Inst
           href='https://www.instagram.com/misha_kompaniec'
           target='_blank'
           rel='noopener noreferrer'
         >
-          {' '}
           {t('instagram.title2')}
         </Inst>
       </Title>
-      <FlexWrapper>
-        <Image1 src='/images/image1.jpg' alt='Image 1' />
-        <ImgWrapper>
-          <ImgBlock>
-            <ImageItem src='/images/image2.jpg' alt='Image 2' />
-            <ImageItem src='/images/image3.jpg' alt='Image 3' />
-          </ImgBlock>
-          <ImgBlock>
-            <ImageItem src='/images/image4.jpg' alt='Image 4' />
-            <ImageItem src='/images/image5.jpg' alt='Image 5' />
-          </ImgBlock>
-        </ImgWrapper>
-      </FlexWrapper>
-    </>
+      <Grid>
+        <LargeImage src='/images/image1.jpg' alt='Image 1' />
+        <SmallImage src='/images/image2.jpg' alt='Image 2' />
+        <SmallImage src='/images/image3.jpg' alt='Image 3' />
+        <SmallImage src='/images/image4.jpg' alt='Image 4' />
+        <SmallImage src='/images/image5.jpg' alt='Image 5' />
+      </Grid>
+    </Container>
   );
 };
 
--- src/pages/home/instagram/style.ts
+++ src/pages/home/instagram/style.ts
@@ -1,27 +1,21 @@
 import { colors } from '@/theme/colors';
 import styled from 'styled-components';
 
-export const FlexWrapper = styled.div`
-  display: flex;
-  gap: 10px;
+export const Container = styled.div`
+  max-width: 1200px;
   margin: 0 auto;
-  padding: 30px 25px 50px 25px;
-  flex-wrap: wrap;
-  justify-content: center;
+  padding: 30px 20px 50px 20px;
 
   @media (max-width: 768px) {
-    flex-direction: column;
-    align-items: center;
     padding: 20px 15px 40px 15px;
   }
 `;
 
 export const Title = styled.h1`
-  width: fit-content;
-  margin: 0 auto;
   font-size: 4vw;
   color: ${colors.black};
   text-align: center;
+  margin-bottom: 40px;
 
   @media (max-width: 768px) {
     font-size: 6vw;
@@ -32,62 +26,65 @@ export const Title = styled.h1`
   }
 `;
 
-export const Image1 = styled.img`
-  width: 490px;
-  height: 500px;
-  object-fit: cover;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(2, 250px);
+  gap: 5px;
+
+  @media (max-width: 1024px) {
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(2, 200px);
+    gap: 5px;
+  }
 
   @media (max-width: 768px) {
-    width: 100%;
-    height: auto;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(3, 200px);
   }
 
   @media (max-width: 480px) {
-    max-width: 100%;
-    height: auto;
+    grid-template-columns: 1fr;
+    grid-template-rows: auto;
+    gap: 5px;
   }
 `;
 
-export const ImageItem = styled.img`
-  width: 245px;
-  height: 245px;
+export const LargeImage = styled.img`
+  width: 100%;
+  height: 100%;
   object-fit: cover;
+  grid-column: span 2;
+  grid-row: span 2;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 
-  @media (max-width: 768px) {
-    width: 48%;
-    height: auto;
+  @media (max-width: 1024px) {
+    grid-column: span 2;
+    grid-row: span 1;
+    height: 100%;
   }
 
   @media (max-width: 480px) {
-    width: 100%;
+    grid-column: 1 / -1;
+    grid-row: auto;
     height: auto;
   }
 `;
 
-export const ImgWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
+export const SmallImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 
-  @media (max-width: 768px) {
-    width: 100%;
-  }
-`;
-
-export const ImgBlock = styled.div`
-  display: flex;
-  gap: 10px;
-
-  @media (max-width: 768px) {
-    justify-content: space-between;
-    flex-wrap: wrap;
-    gap: 10px;
+  @media (max-width: 480px) {
+    height: auto;
   }
 `;
 
 export const Inst = styled.a`
+  display: inline-block;
+  margin-top: 10px;
   text-decoration: none;
   cursor: pointer;
   color: ${colors.pink};
