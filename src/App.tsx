3964e35f302a8d11703f17aa60571e66afe716c8
Misha Kompaniec <mikokompaniec@gmail.com>
2025-07-22 15:31:16 +0200

    change provider logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,22 @@ import { BrowserRouter } from 'react-router-dom';
 import { Layout } from './components/layout';
 import Routes from './router';
 import ScrollToTop from './utils/ScrollToTop';
-import AppProviders from './context/appProviders';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import { NotificationProvider } from './context/notificationContext';
 
 const App = () => {
   return (
-    <AppProviders>
-      <BrowserRouter>
-        <Layout>
-          <ScrollToTop />
-          <Routes />
-        </Layout>
-      </BrowserRouter>
-    </AppProviders>
+    <Provider store={store}>
+      <NotificationProvider>
+        <BrowserRouter>
+          <Layout>
+            <ScrollToTop />
+            <Routes />
+          </Layout>
+        </BrowserRouter>
+      </NotificationProvider>
+    </Provider>
   );
 };
 
