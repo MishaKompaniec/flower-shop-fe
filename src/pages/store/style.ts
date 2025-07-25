70775e620ed0887f0afa0a8918fe25f928f133c6
Misha Kompaniec <mikokompaniec@gmail.com>
2025-07-25 13:03:16 +0200

    change tab styles

--- src/pages/store/style.ts
+++ src/pages/store/style.ts
@@ -6,6 +6,13 @@ export const Wrapper = styled.div`
   padding-top: 50px;
   background-color: ${colors.pinkBg};
 
+  .ant-tabs .ant-tabs-tab {
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 700;
+    user-select: none;
+    color: ${colors.pink};
+  }
+
   @media (max-width: 768px) {
     & .ant-tabs-nav {
       padding: 0 15px 0 15px;
